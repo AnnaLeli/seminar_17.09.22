a36942ffd7f34c603c2076566e4a47a465c2fab2
LA <annleli@yandex.ru>
2022-09-17 22:56:07 +0300

    002

---
+++ 002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите значение: a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение: b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if 
+    (b * b == a)
+    Console.WriteLine("Yes");
+else 
+    Console.WriteLine("No");
+
+
