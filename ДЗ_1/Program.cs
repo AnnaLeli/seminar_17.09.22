92c64c263a550384766943bb1e349dd976ba5e31
LA <annleli@yandex.ru>
2022-09-18 00:08:41 +0300

    задача 2

---
+++ ДЗ_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите значение: a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение: b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if 
+    (a > b)
+    Console.WriteLine("MAX " + a);
+else 
+    Console.WriteLine("MAX " + b);
+
+
+
