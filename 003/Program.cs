97bd150455757102eed3560b64f5e65db0c9b9e5
LA <annleli@yandex.ru>
2022-09-17 23:18:31 +0300

    003

---
+++ 003/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+Console.Write("Введите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if  
+(a == 1)
+Console.Write("Monday");
+else
+    if 
+    (a == 2)
+    Console.Write("tuesday");
+    else
+        if 
+        (a == 3)
+        Console.Write("Wednesday");
+        else
+            if 
+            (a == 4)
+            Console.Write("thursday");
+            else
+                if 
+                (a == 5)
+                Console.Write("friday");
+                else
+                    if 
+                    (a == 6)
+                    Console.Write("saturday");
+                    else
+                    Console.Write("sunday");
