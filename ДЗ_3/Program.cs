ff540a3f580577dc87e2ae348868d1c443d9eea9
LA <annleli@yandex.ru>
2022-09-18 00:18:33 +0300

    задача 6

---
+++ ДЗ_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if
+    (n % 2 == 0)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
